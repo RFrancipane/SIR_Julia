ec0a6fa63e3ab41daec00a50c321758b5e4f4fa8
Raphael Francipane <ruffif@gmail.com>
2024-09-26 13:24:36 +1000

    changes!

--- src/SIR_pkg.jl
+++ src/SIR_pkg.jl
@@ -1,5 +1,14 @@
 module SIR_pkg
 
+using Pkg
+using Revise
+using Plots
+using DifferentialEquations
 # Write your package code here.
+
+export simulate_model
+export plot_solution
+export time_cdf_gamma
+include("SIR.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "SIR_pkg.jl" begin
     # Write your tests here.
+    @test time_cdf_gamma(10,0.5) == 0.06931471805599453
+    @test time_cdf_gamma(1,1) == Inf
+    @test time_cdf_gamma(1,0) == 0
+
 end
